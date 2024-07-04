9e9d295a49352442c0b697aca15281fac96fa0f5
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-07-04 15:19:00 +0600

    added Item and categories model

--- item/models.py
+++ item/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by=models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    def __str__(self):
+        return self.name 
--- marketcore/views.py
+++ marketcore/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 def index(request):
-    return render(request, 'core/index.html')
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False).order_by('created_at')[0:6]
+    return render(request, 'core/index.html',{
+            'categories': categories,
+            'items': items
+        })
 
 def contact(request):
     return render(request, 'core/contact.html')
