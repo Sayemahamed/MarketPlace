1d3bf581ad670d60b691bda9f7c630c5eebc68e5
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-07-06 12:10:05 +0600

    implemented browse item feature

--- item/views.py
+++ item/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404,redirect
 from .models import Item
-from .forms import NewProductForm
+from .forms import NewProductForm, EditProductForm
 
 
+def search(request):
+    query = request.GET.get('query', '')
+    items = Item.objects.filter(is_sold=False)
+    if query:
+        items = items.filter(name__icontains=query,is_sold=False)
+    return render(request, 'item/search.html',{
+        'items': items,
+        'query': query
+    })
 def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item_id)
@@ -24,4 +33,25 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form ,
         'title': 'New Product'
+    })
+
+@login_required
+def delete(request, item_id):
+    item = get_object_or_404(Item, pk=item_id, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index')
+
+@login_required
+def edit(request, item_id):
+    item = get_object_or_404(Item, pk=item_id, created_by=request.user)
+    if request.method == 'POST':
+        form = EditProductForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item = form.save()
+            return redirect('item:detail', item.id)
+    else:
+        form = EditProductForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Product'
     })
